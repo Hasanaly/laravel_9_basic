dc56712762a73a3821b28882c1cb31311e0d55b6
Ahmad Ali Hasan <hasanaly4u@gmail.com>
2023-04-30 15:14:28 +0700

    Route Basic Clear

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PMI Kota Semarang</title>
+</head>
+<body>
+    <h1>Informasi Kontak</h1>
+    <h2>{{ $name }}</h2>
+    <h2>{{ $phone }}</h2>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,69 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//menampilkan langsung hasil perhitunga
+Route::get('/hitung', function() {
+    return 9*9;
+});
+
+//menampilkan langsung string
+Route::get('/string', function() {
+    return "apa saja";
+});
+
+//Penulisan Route Basic Multi Argument
+Route::get('/kontak', function() {
+    return view('kontak',['name'=>'Ahmad Ali Hasan','phone'=>'08770032.....']);
+});
+
+//Penulisan Route Basic Multi Parameter
+// Route::get('/kontak', function() {
+//     return view('kontak');
+// });
+
+//Penulisan Route Simple
+// Route::view('/kontak','kontak');
+
+//Penulisan Route Simple dengan Argument
+// Route::view('/kontak', 'kontak',['name'=>'Ahmad Ali Hasan']);
+// CASESENSTITIVE -> name pada route menjadi variabel $name pada view
+
+//Penulisan Route Simple dengan Multi Argument
+// Route::view('/kontak', 'kontak',['name'=>'Ahmad Ali Hasan','phone'=>'087700325770']);
+
+//REDIRECT Route::redirect('/here','/there');
+//Route::redirect('/kontak','/string');
+
+//Route dengan Parameter Dinamis
+Route::get('/produk/{id}', function($id) {
+    return 'ini adalah produk dengan id '.$id;
+});
+
+//Route ke View dengan Parameter Dinamis
+Route::get('/produk/{id}', function ($id) {
+ return view('produk.detail',['id'=>$id]);
+});
+
+//PREFIX Routing
+// Route::get('admin/profil', function(){
+//     return 'profil admin';
+// });
+// Route::get('admin/tentang', function(){
+//     return 'tentang admin';
+// });
+// Route::get('admin/kontak', function(){
+//     return 'kontak admin';
+// });
+//MENJADI
+Route::prefix('admin')->group(function(){
+    Route::get('profil', function(){
+        return 'profil admin';
+    });
+    Route::get('tentang', function(){
+        return 'tentang admin';
+    });
+    Route::get('kontak', function(){
+        return 'kontak admin';
+    });
+});
